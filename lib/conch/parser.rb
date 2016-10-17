ba28f778aaf7c29b67cb85b7ab3f289e516f6be7
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2016-10-17 20:30:32 +1100

    Added TODOs

--- lib/conch/parser.rb
+++ lib/conch/parser.rb
@@ -2,6 +2,7 @@ module Conch
   class Parser
 
     def self.parse_to_ast(string)
+      # TODO
     end
 
   end
--- lib/conch/virtual_machine.rb
+++ lib/conch/virtual_machine.rb
@@ -2,6 +2,7 @@ module Conch
   class VirtualMachine
 
     def self.evaluate(tokens)
+      # TODO
     end
 
   end
